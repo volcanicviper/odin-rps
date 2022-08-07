a458799ff3c7ee1d5287a522b0e2cc1b262599ee
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-07 15:42:34 +0200

    Added selection prompt

--- rps.js
+++ rps.js
@@ -1,5 +1,14 @@
-const rps = ['Rock', 'Paper', 'Scissors'];
+const rps = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     return rps[Math.floor(Math.random() * 3)]; // pick num from 0 to 2 then print string from array
 }
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+                                            /* convert to lowercase in order to make it work
+                                                regardless of capitalization */
+playRound(playerSelection, getComputerChoice());
